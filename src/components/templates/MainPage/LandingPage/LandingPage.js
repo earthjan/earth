8c37ec0003b89f86966b08ba6a77ba0ec38178a2
= <=>
2022-10-29 23:15:46 +0800

    Extract  landing page

---
+++ src/components/templates/MainPage/LandingPage/LandingPage.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+import { Grid, Box, Typography, IconButton, Stack } from "@mui/material";
+import SouthIcon from "@mui/icons-material/South";
+
+import GridItem from "../../../atoms/GridItem/GridItem";
+import TextGray from "../../../atoms/TextGray/TextGray";
+
+import styles from "./styles";
+
+const LandingPage = () => {
+  return (
+    <Grid container sx={styles.gridBase}>
+      <GridItem sx={styles.upperSubtitleBase}>
+        <TextGray sx={styles.subtitleUpper}>Earth Jan Baquir Marzan</TextGray>
+      </GridItem>
+      <GridItem sx={styles.mainTextBase}>
+        <MainContent />
+      </GridItem>
+      <GridItem container={true} sx={styles.bottomSubtitleBase}>
+        <TextGray sx={styles.subtitleBottom}>
+          I build apps with modern techs, ensuring reliablity and
+          maintainability.
+        </TextGray>
+      </GridItem>
+
+      <Box sx={styles.scrollDownBase}>
+        <IconButton>
+          <Stack sx={styles.scrollDownStack}>
+            <GridItem container={true} sx={styles.scrollDownIconBase}>
+              <SouthIcon sx={styles.scrollDownIcon} />
+            </GridItem>
+            <Typography sx={styles.scrollDownLabel}>Scroll Down</Typography>
+          </Stack>
+        </IconButton>
+      </Box>
+      <Box sx={styles.extraBg} />
+    </Grid>
+  );
+};
+
+export default LandingPage;
+
+// =============== HELPER COMPONENTS/FUNCTIONS ===============
+
+function Span({ text }) {
+  return (
+    <Typography component="span" sx={styles.mainTextContentSpan}>
+      {" "}
+      {text}{" "}
+    </Typography>
+  );
+}
+
+function MainContent() {
+  return (
+    <GridItem container={true}>
+      <GridItem container={true} sx={styles.mainTextContentParent}>
+        <Typography sx={styles.mainTextContent}>
+          Full-stack <Span text="Web Developer" />
+        </Typography>
+      </GridItem>
+      <GridItem container={true} sx={styles.mainTextContentParent}>
+        <Typography sx={styles.mainTextContent}>
+          with
+          <Span text="Bachelor Degree" />
+          in
+          <Span text="IT" />
+        </Typography>
+      </GridItem>
+    </GridItem>
+  );
+}
--- src/components/templates/MainPage/styles.js
+++ src/components/templates/MainPage/styles.js
@@ -1,5 +1,5 @@
 const styles = {
-  landingPageBase: {
+  mainPageBase: {
     backgroundColor: "primary.main",
     minWidth: "320px",
     height: {
@@ -12,12 +12,8 @@ const styles = {
     display: { xl: "flex" },
     justifyContent: { xl: "center" },
   },
+
   maxWidthSetter: {
-    /**
-     * Used by extra background to emphasize the landing page text
-     */
-    position: "relative",
-    zIndex: "1",
     maxWidth: "1536px",
 
     /**
@@ -26,158 +22,6 @@ const styles = {
     border: 1,
     borderColor: "white",
   },
-
-  gridBase: {
-    px: {
-      xs: "15px",
-    },
-    paddingTop: {
-      xs: "30vh",
-      sm: "40vh",
-      md: "30vh",
-      lg: "20vh",
-    },
-  },
-
-  mainTextContentParent: {
-    justifyContent: {
-      xs: "flex-start",
-      md: "center",
-    },
-  },
-  mainTextContent: {
-    fontSize: {
-      xs: "2rem",
-      md: "2rem",
-      lg: "2.5rem",
-      xl: "30px",
-    },
-  },
-  mainTextContentSpan: {
-    fontWeight: {
-      xs: "medium",
-    },
-    fontSize: {
-      xs: "2.5rem",
-      md: "4rem",
-      lg: "6rem",
-      xl: "8rem",
-    },
-  },
-
-  upperSubtitleBase: {
-    paddingLeft: getSubtitlePaddingX(),
-  },
-  subtitleUpper: {
-    fontSize: getSubtitleFontSize(),
-  },
-
-  bottomSubtitleBase: {
-    justifyContent: {
-      md: "flex-end",
-    },
-    paddingTop: {
-      md: "10px",
-    },
-    paddingRight: getSubtitlePaddingX(),
-  },
-
-  subtitleBottom: {
-    fontSize: getSubtitleFontSize(),
-    width: {
-      md: "30vw",
-      xl: "400px",
-    },
-  },
-
-  scrollDownBase: {
-    position: "absolute",
-    top: "90vh",
-    left: {
-      xs: "40vw",
-      sm: "45vw",
-      /**
-       * Instead of vw, it depends on the set max width.
-       */
-      xl: "50%",
-    },
-  },
-  scrollDownStack: {
-    flexDirection: "column",
-  },
-  scrollDownIconBase: {
-    justifyContent: "center",
-  },
-  scrollDownIcon: {
-    position: "absolute",
-    fontSize: {
-      xs: "0.8rem",
-    },
-    color: "titleText.main",
-    animation: "move 1s linear infinite alternate",
-    "@keyframes move": {
-      "0%": {
-        top: "5px",
-      },
-      "100%": {
-        top: "10px",
-      },
-    },
-  },
-  scrollDownLabel: {
-    fontSize: {
-      xs: "0.5rem",
-    },
-    color: "content.main",
-    marginTop: "15px",
-  },
-
-  extraBg: {
-    /**
-     * Hides it on <md screens as it's harder than I thought to keep it on the same spot.
-     */
-    display: {
-      xs: "none",
-      md: "block",
-    },
-    position: "absolute",
-    height: {
-      md: "300px",
-    },
-    width: {
-      md: "60%",
-      xl: "75%"
-    },
-    backgroundColor: "secondary.main",
-    top: { 
-      md: "30vh", 
-      lg: "20vh", 
-      xl: "25vh" 
-    },
-    left: { 
-      xs: "25vw", 
-      md: "35vw", 
-      xl: "25%" 
-    },
-    zIndex: "-1",
-    borderRadius: "5px",
-  },
 };
 
 export default styles;
-
-// =================== HELPER FUNCTIONS ===================
-
-function getSubtitleFontSize() {
-  return {
-    sm: "0.8rem",
-    lg: "1rem",
-  };
-}
-
-function getSubtitlePaddingX() {
-  return {
-    md: "60px",
-    lg: "70px",
-  };
-}
