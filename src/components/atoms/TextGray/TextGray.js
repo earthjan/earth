b7e4a9c551384a46586f61d43ce0781d5b88696f
= <=>
2022-11-01 00:36:56 +0800

    Add `component` prop to `TextGray`

--- src/components/atoms/TextGray/TextGray.js
+++ src/components/atoms/TextGray/TextGray.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { Typography } from '@mui/material'
 
-const TextGray = ({ children, sx }) => {
+const TextGray = ({ component, children, sx }) => {
   return (
-    <Typography color="content.main" sx={sx}>{children}</Typography>
+    <Typography component={component} color="content.main" sx={sx}>{children}</Typography>
   )
 }
 
