84d2a6ee2e00eb7c0a09ab5f93ed6befbd4084d3
= <=>
2022-10-31 23:39:39 +0800

    Change back the `TextWithIcon`'s styles

--- src/components/templates/MainPage/styles.js
+++ src/components/templates/MainPage/styles.js
@@ -37,6 +37,20 @@ const styles = {
   },
 
   bioPadding: { px: "15px" },
+
+  textWithIconBase: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  textWithIconText: {
+    textDecoration: "underline",
+    color: "links.main",
+  },
+  textWithIconIcon: {
+    color: "links.main",
+    paddingLeft: "15px",
+  },
 };
 
 export default styles;
