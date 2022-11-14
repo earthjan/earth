d54ff6f0740c997cffad435e8064b66cc2aa0756
= <=>
2022-11-14 21:49:12 +0800

    Document the custom textfield

--- src/components/atoms/TextField/TextField.js
+++ src/components/atoms/TextField/TextField.js
@@ -6,6 +6,35 @@ import { TextField as MUITextField, InputAdornment } from "@mui/material";
 
 import styles from "./styles";
 
+/**
+ * A custom textfield
+ * 
+ * @param {object} props
+ * @param {string | object | ReactElement} props.label - Input label
+ * @param {string | object} props.variant - Textfield variant
+ * @param {function} props.onChange - onChange handler
+ * 
+ * @example 
+ * // Basic usage
+ * (
+ *  // With simple `label`
+ *  <TextField label="Name:" />
+ * 
+ *  // With complex `label`
+ *  <TextField
+        label={
+          <Typography>
+            Subject
+            <Typography component="span" fontSize="0.7rem">
+              {" "}
+              (optional)
+            </Typography>
+            :
+          </Typography>
+        }
+    />
+ * )
+ */
 const TextField = ({ label, variant, onChange }) => {
   return (
     <MUITextField
@@ -24,7 +53,11 @@ const TextField = ({ label, variant, onChange }) => {
 };
 
 TextField.propTypes = {
-  label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.element,
+  ]),
   variant: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   onChange: PropTypes.func,
 };
