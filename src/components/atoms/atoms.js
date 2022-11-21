9dac133fbece4a5fec4816c0f1c5e4d83273826c
= <=>
2022-11-21 20:38:05 +0800

    Add atomic component export

--- src/components/atoms/atoms.js
+++ src/components/atoms/atoms.js
@@ -1,5 +1,6 @@
 import TextWithIcon from "./TextWithIcon/TextWithIcon";
 import Menu from "./Menu/Menu";
 import NavButton from "./NavButton/NavButton";
+import LogoContainer from "./LogoContainer/LogoContainer";
 
-export { TextWithIcon, Menu, NavButton }
+export { TextWithIcon, Menu, NavButton, LogoContainer }
