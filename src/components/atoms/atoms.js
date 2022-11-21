99fe4955c2ffe64b2ca2be1bf4568e9c7f684a7b
= <=>
2022-11-21 21:58:16 +0800

    Add atom component exports

--- src/components/atoms/atoms.js
+++ src/components/atoms/atoms.js
@@ -2,5 +2,18 @@ import TextWithIcon from "./TextWithIcon/TextWithIcon";
 import Menu from "./Menu/Menu";
 import NavButton from "./NavButton/NavButton";
 import LogoContainer from "./LogoContainer/LogoContainer";
+import TextTitle from "./TextTitle/TextTitle";
+import TextGray from "./TextGray/TextGray";
+import GridItem from "./GridItem/GridItem";
+import TextLink from "./TextLink/TextLink";
 
-export { TextWithIcon, Menu, NavButton, LogoContainer }
+export {
+  TextWithIcon,
+  Menu,
+  NavButton,
+  LogoContainer,
+  TextTitle,
+  TextGray,
+  GridItem,
+  TextLink
+};
